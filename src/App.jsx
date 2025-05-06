d525bea7b26dfa6db20ad01243bc9767b8b37f09
Sam788778 <samvelabrahamyan566@gmail.com>
2025-05-06 21:16:51 +0400

    1: Create base React app and install Formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,62 @@
 import './App.css'
-import Container from './components/Container/container'
+import { Formik } from 'formik'
 
 function App() {
-
   return (
-    <>
-      <Container />
-    </>
+    <div className="container">
+      <Formik
+        initialValues={{
+          name: '',
+          lastName: '',
+          email: '',
+          password: '',
+        }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ handleChange, handleSubmit, values }) => (
+          <>
+            <div className="live-preview">
+              <h3>Live Preview:</h3>
+              <p>Name: {values.name}</p>
+              <p>Last Name: {values.lastName}</p>
+              <p>Email: {values.email}</p>
+            </div>
+
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                onChange={handleChange}
+                value={values.name}
+              />
+              <input
+                type="text"
+                name="lastName"
+                placeholder="Last Name"
+                onChange={handleChange}
+                value={values.lastName}
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                onChange={handleChange}
+                value={values.email}
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={handleChange}
+                value={values.password}
+              />
+              <button type="submit">Submit</button>
+            </form>
+          </>
+        )}
+      </Formik>
+    </div>
   )
 }
 
